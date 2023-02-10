4dd5e5795f0ae0e362d4069283251977624a4745
wangxu <xu.wang@nx-engine.com>
2023-02-10 18:02:41 +0800

    1

--- uniCloud-aliyun/cloudfunctions/home/dto/account/get.js
+++ uniCloud-aliyun/cloudfunctions/home/dto/account/get.js
@@ -26,35 +26,34 @@ module.exports.get = async ctx => {
   const dbJQL = uniCloud.databaseForJQL({ event, context });
   const Account = dbJQL.collection("qie-account");
 
-  // const res = Account.where({ uid })
-  //   .groupBy('dateToString(add(new Date(0),last_update_date),"%Y-%m-%d-%u") as date') // 分组字段
-  //   .groupField(
-  //     `count(*) as total,
-  //       sum(money) as all_money,
-  //       push({money:money, account_type:account_type, remark:remark, bill_type:bill_type, date:date,last_update_date:last_update_date}) as list`
-  //   ) // 数据分组
-  //   .orderBy("list.last_update_date", "desc") // 时间倒序
-  //   .skip(skip) // 跳过前n条
-  //   .limit(limit) // 获取n条
-  //   .get();
-
-  const res = Account.aggregate()
-    .where({ uid })
-    .sort({ last_update_date: 1 })
-    // .field(
-    //   'money, account_type, remark, bill_type, last_update_date, dateToString(add(new Date(0),last_update_date),"%Y-%m-%d-%u") as date'
-    // ) // 字段处理
-
+  const res = Account.where({ uid })
     .groupBy('dateToString(add(new Date(0),last_update_date),"%Y-%m-%d-%u") as date') // 分组字段
     .groupField(
       `count(*) as total,
         sum(money) as all_money,
         push({money:money, account_type:account_type, remark:remark, bill_type:bill_type, date:date,last_update_date:last_update_date}) as list`
     ) // 数据分组
-    .orderBy("last_update_date", "desc") // 时间倒序
+    .orderBy("list.last_update_date", "desc") // 时间倒序
     .skip(skip) // 跳过前n条
     .limit(limit) // 获取n条
     .get();
+
+  // const res = Account.aggregate()
+  //   .where({ uid })
+  //   // .field(
+  //   //   'money, account_type, remark, bill_type, last_update_date, dateToString(add(new Date(0),last_update_date),"%Y-%m-%d-%u") as date'
+  //   // ) // 字段处理
+
+  //   .groupBy('dateToString(add(new Date(0),last_update_date),"%Y-%m-%d-%u") as date') // 分组字段
+  //   .groupField(
+  //     `count(*) as total,
+  //       sum(money) as all_money,
+  //       push({money:money, account_type:account_type, remark:remark, bill_type:bill_type, date:date,last_update_date:last_update_date}) as list`
+  //   ) // 数据分组
+  //   .orderBy("last_update_date", "desc") // 时间倒序
+  //   .skip(skip) // 跳过前n条
+  //   .limit(limit) // 获取n条
+  //   .get();
 
   // const accountData = res.data;
 
